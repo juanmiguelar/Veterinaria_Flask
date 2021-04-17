7f18e69fd70294a5c47a91c216e144fdd3283ffb
Juan Miguel <juanmiguelar09@gmail.com>
2021-04-17 22:58:04 +0000

    Inicio

--- src/main.py
+++ src/main.py
@@ -8,17 +8,25 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Mascota
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+
+jwt = JWTManager(app)
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -31,13 +39,45 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
-
+def GetUser():
     response_body = {
         "msg": "Hello, this is your GET /user response "
     }
-
     return jsonify(response_body), 200
+
+@app.route('/login', methods=['POST'])
+def Login():
+    # OBTENIENDO LOS DATOS
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    # FILTRANDO POR EMAIL Y PASSWORD
+    # FIRST() PARA ELIMINAR EL DUPLICADO DE DATOS
+    user = User.query.filter_by(email=email, password=password).first()
+    if user is None:
+        # the user was not found on the database
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    # create a new token with the user id inside
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id })
+
+@app.route('/users', methods=['GET'])
+def GetAllUsers():
+    users = User.query.all()
+    responseUsers = list(map(lambda user: user.serialize(), users))
+    return jsonify(responseUsers), 200
+
+@app.route('/mascota/<int:idUsuario>', methods=['GET'])
+def GetMascotaByIdUser(idUsuario):
+    mascotasDelUsuario = Mascota.query.filter_by(idUser=idUsuario)
+    """
+    RespuestaMascotas = mascotas.map((mascota)=>{
+        return mascota.serialize();
+    }) 
+    """
+    respuestaMascotas = list(map(lambda mascota: mascota.serialize(), mascotasDelUsuario))
+    return jsonify(respuestaMascotas), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -6,10 +6,9 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
@@ -17,3 +16,26 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Mascota(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    idUser = db.Column(db.Integer, db.ForeignKey('user.id'))
+    nombre = db.Column(db.String(120), unique=False, nullable=False)
+    edad = db.Column(db.Integer, nullable=False)
+    peso = db.Column(db.Integer, nullable=False)
+    user = db.relationship("User", backref=db.backref("Mascota", cascade="all,delete"))
+
+    def __repr__(self):
+        return '<Mascota %r>' % self.nombre
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "idUser": self.idUser,
+            "nombre": self.nombre,
+            "edad": self.edad,
+            "peso": self.peso,
+            "user": self.user.serialize()
+            # do not serialize the password, its a security breach
+        }
+    
